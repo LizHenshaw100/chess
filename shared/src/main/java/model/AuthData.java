87ce5f2e012fd33894f8651c69f0447c9b8e6232
Liz Henshaw <elizabethhenshaw170@gmail.com>
2026-02-24 19:07:00 -0700

    Added model package with data classes

---
+++ shared/src/main/java/model/AuthData.java
@@ -0,0 +1,13 @@
+package model;
+
+public class AuthData {
+    private final String authToken;
+    private final String username;
+
+
+    AuthData(String authToken, String username) {
+
+        this.authToken = authToken;
+        this.username = username;
+    }
+}
---
+++ shared/src/main/java/model/UserData.java
@@ -0,0 +1,14 @@
+package model;
+
+class UserData {
+    private final String username;
+    private final String password;
+    private final String email;
+
+    UserData(String username, String password, String email) {
+
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
