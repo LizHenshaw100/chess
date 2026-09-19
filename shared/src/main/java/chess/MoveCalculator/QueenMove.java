d020ab03b7001cdb4054588d18d42c709e6fee59
Liz <elizabeth@gosameday.com>
2026-09-19 17:31:33 -0600

    adjusted queen movements

--- shared/src/main/java/chess/MoveCalculator/QueenMove.java
+++ shared/src/main/java/chess/MoveCalculator/QueenMove.java
@@ -10,7 +10,9 @@ public class QueenMove implements MoveCalculatorInterface{
     @Override
     public ArrayList<ChessMove> getMoves(ChessBoard board, ChessPosition pos) {
         ArrayList<ChessMove> moves = new ArrayList<>();
+        //get diagonal movements from bishop
         MoveCalculatorInterface diags = new BishopMove();
+        //get diagonal movements from rook
         MoveCalculatorInterface straights = new RookMove();
         ArrayList<ChessMove> diagMoves = diags.getMoves(board, pos);
         ArrayList<ChessMove> straightMoves = straights.getMoves(board, pos);
